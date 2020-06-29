81ba278e50c95ab82b2b853018c398e31a86afd9
Ynda Jas <yndajas@gmail.com>
2020-06-29 09:48:44 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,29 @@
+def input_to_index(input)
+  converted_input = input.to_i - 1
+  converted_input
+end
+
+def position_taken?(board,index)
+  board[index] != " " && board[index] != "" && board[index] != nil
+end
+
+def valid_move?(board,index)
+index.between?(0,8) && !position_taken?(board,index)
+end
+
+def move(board,player_move,player="X")
+  board[player_move] = player
+  board
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+end
